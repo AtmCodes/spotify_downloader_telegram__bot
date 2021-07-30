c87008b6b9db5f57ffa33b80f5d4fa98e8ae85a9
AtmCodes <88212574+AtmCodes@users.noreply.github.com>
2021-07-30 16:49:08 -0400

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -14,7 +14,7 @@ spotifyy = spotipy.Spotify(
                                                         client_secret='389614e1ec874f17b8c99511c7baa2f6'))
 genius = lyricsgenius.Genius('biZZReO7F98mji5oz3cE0FiIG73Hh07qoXSIzYSGNN3GBsnY-eUrPAVSdJk_0_de')
 
-token = 'token bot'
+token = '1930834520:AAEBeg-Ck_rTxg-0pivcveDiSzFkNWGABDY'
 
 bot = telepot.Bot(token)
 
